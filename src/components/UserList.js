3e183508404703ef583dc9f399f741e656559052
ziyadh95 <99491030+ziyadh95@users.noreply.github.com>
2022-08-17 23:37:12 +0700

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -10,13 +10,13 @@ const UserList = () => {
   }, []);
  
   const getUsers = async () => {
-    const response = await axios.get("http://localhost:5000/users");
+    const response = await axios.get("https://ziya-uas-backend.herokuapp.com/users");
     setUser(response.data);
   };
  
   const deleteUser = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/users/${id}`);
+      await axios.delete(`https://ziya-uas-backend.herokuapp.com/users/${id}`);
       getUsers();
     } catch (error) {
       console.log(error);
